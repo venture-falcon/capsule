76349f8a64392366f2dde0f9b8b5f7723e95155b
Anton Österberg <anton.osterberg@protonmail.com>
2023-05-03 16:14:01 +0200

    Make class Memoized internal

--- lib/src/main/kotlin/io/nexure/capsule/Memoized.kt
+++ lib/src/main/kotlin/io/nexure/capsule/Memoized.kt
@@ -2,7 +2,7 @@ package io.nexure.capsule
 
 import java.util.concurrent.Semaphore
 
-class Memoized<T> {
+internal class Memoized<T> {
     private val semaphore = Semaphore(1)
     private var value: T? = null
 
